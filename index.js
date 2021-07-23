30edfe34388dbf591a110b960fcb7a2ce1b5d800
Rachealelias <rachealsamuel708@gmail.com>
2021-07-23 15:38:13 -0400

    commit tree 1fd112ec1269fe64167e46f560dfb0835af54dd1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM();
+  });
+  function updateDOM(){
+      const text = document.getElementById('text');
+      //console.log(text);
+      text.innerHTML = 'This is really cool!';
+  }
+  
+  
+
+
+
+
+
